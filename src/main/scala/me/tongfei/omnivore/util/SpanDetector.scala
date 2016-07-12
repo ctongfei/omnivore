c42cfc743e1b51f3e7e7cd01cc8867113d2a496d
ctongfei <ctongfei@gmail.com>
2016-07-12 16:52:21 -0400

    span detector

---
+++ src/main/scala/me/tongfei/omnivore/util/SpanDetector.scala
@@ -0,0 +1,18 @@
+package me.tongfei.omnivore.util
+
+/**
+ * @author Tongfei Chen
+ */
+class SpanDetector(source: String) {
+
+  private[this] var index = 0
+
+  def spanOf(pattern: String) = {
+    val i = source.substring(index).indexOf(pattern)
+    val res = (index + i, index + i + pattern.length)
+    if (i != -1) // the pattern is actually found
+      index += i + pattern.length
+    (index + i, index + i + pattern.length)
+  }
+
+}
